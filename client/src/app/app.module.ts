226b1ee0ddbe58fe82911ef51684c8606afec999
Ahmad Sameh <41856063+AhmedSamehh@users.noreply.github.com>
2022-01-26 19:23:31 +0200

    added loading spinner

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http'
 
@@ -23,6 +23,7 @@ import { ServerErrorComponent } from './errors/server-error/server-error.compone
 import { MemberCardComponent } from './members/member-card/member-card.component';
 import { JwtInterceptor } from './_interceptors/jwt.interceptor';
 import { MemberEditComponent } from './members/member-edit/member-edit.component';
+import { LoadingInterceptor } from './_interceptors/loading.interceptor';
 
 @NgModule({
   declarations: [
@@ -45,11 +46,13 @@ import { MemberEditComponent } from './members/member-edit/member-edit.component
     HttpClientModule,
     BrowserAnimationsModule,
     FormsModule,
-    SharedModule
+    SharedModule,
   ],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   providers: [
     {provide: HTTP_INTERCEPTORS, useClass:ErrorInterceptor, multi: true},
-    {provide: HTTP_INTERCEPTORS, useClass:JwtInterceptor, multi: true}
+    {provide: HTTP_INTERCEPTORS, useClass:JwtInterceptor, multi: true},
+    {provide: HTTP_INTERCEPTORS, useClass:LoadingInterceptor, multi: true},
   ],
   bootstrap: [AppComponent]
 })
