b5b6b5775e04c0cc784ce00ae9ea834547bd2857
Ahmad Sameh <41856063+AhmedSamehh@users.noreply.github.com>
2022-03-05 20:43:23 +0200

    refractoring and seeding data

--- API/Data/Seed.cs
+++ API/Data/Seed.cs
@@ -2,28 +2,29 @@ using System.Collections.Generic;
 using System.Text.Json;
 using System.Threading.Tasks;
 using API.Entities;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Data
 {
     public class Seed
     {
-        public static async Task SeedUsers(DataContext context){
-            if(await context.Users.AnyAsync())
+        public static async Task SeedUsers(UserManager<AppUser> userManager){
+            if(await userManager.Users.AnyAsync())
                 return;
             var userData = await System.IO.File.ReadAllTextAsync("Data/UserSeedData.json");
             var users = JsonSerializer.Deserialize<List<AppUser>>(userData);
+            if(users == null)
+                return;
+
             foreach (var user in users)
             {
                 // using var hmac = new HMACSHA512();
-                user.UserName = user.UserName.ToLower();
                 // user.PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes("password"));
                 // user.PasswordSalt = hmac.Key;
-
-                context.Users.Add(user);
+                user.UserName = user.UserName.ToLower();
+                await userManager.CreateAsync(user, "Pa$$w0rd");
             }
-
-            await context.SaveChangesAsync();
         } 
     }
 }
--- API/Program.cs
+++ API/Program.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using API.Data;
+using API.Entities;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -14,25 +16,26 @@ namespace API
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
-        //     var host = CreateHostBuilder(args).Build();
-        //     using var scope = host.Services.CreateScope();
-        //     var services = scope.ServiceProvider;
-        //     try
-        //     {
-        //          var context = services.GetRequiredService<DataContext>();
-        //          await context.Database.MigrateAsync();
-        //          await Seed.SeedUsers(context);
-        //     }
-        //     catch (Exception ex)
-        //     {
+            // CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+            using var scope = host.Services.CreateScope();
+            var services = scope.ServiceProvider;
+            try
+            {
+                 var context = services.GetRequiredService<DataContext>();
+                 var userManager = services.GetRequiredService<UserManager<AppUser>>();
+                 await context.Database.MigrateAsync();
+                 await Seed.SeedUsers(userManager);
+            }
+            catch (Exception ex)
+            {
                 
-        //         var logger = services.GetRequiredService<ILogger<Program>>();
-        //         logger.LogError(ex, "An error occured during migration");
-        //     }
-        //    await host.RunAsync();
+                var logger = services.GetRequiredService<ILogger<Program>>();
+                logger.LogError(ex, "An error occured during migration");
+            }
+           await host.RunAsync();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
