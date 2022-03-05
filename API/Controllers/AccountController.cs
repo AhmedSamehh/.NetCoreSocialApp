8bda429580ee5625da9c71214653c8d31f4fd766
Ahmad Sameh <41856063+AhmedSamehh@users.noreply.github.com>
2022-03-05 18:44:49 +0200

    Identity and role mngmnt / updating entities

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -35,11 +35,11 @@ namespace API.Controllers
 
             var user = _mapper.Map<AppUser>(registerDto);
 
-            using var hmac = new HMACSHA512();
+            // using var hmac = new HMACSHA512();
 
             user.UserName = registerDto.Username.ToLower();
-            user.PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDto.Password));
-            user.PasswordSalt = hmac.Key;
+            // user.PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDto.Password));
+            // user.PasswordSalt = hmac.Key;
 
             _context.Users.Add(user);
             await _context.SaveChangesAsync();
@@ -63,15 +63,15 @@ namespace API.Controllers
             if(user == null) 
                 return Unauthorized("Invalid Username");
             
-            using var hmac = new HMACSHA512(user.PasswordSalt);
+            // using var hmac = new HMACSHA512(user.PasswordSalt);
 
-            var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(loginDto.Password));
+            // var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(loginDto.Password));
 
-            for(int i = 0; i<computedHash.Length; i++)
-            {
-                if(computedHash[i] != user.PasswordHash[i]) 
-                    return Unauthorized("Invalid Password");
-            }
+            // for(int i = 0; i<computedHash.Length; i++)
+            // {
+            //     if(computedHash[i] != user.PasswordHash[i]) 
+            //         return Unauthorized("Invalid Password");
+            // }
 
             return new UserDto
             {
